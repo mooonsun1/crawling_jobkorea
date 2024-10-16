b5547e4d0a22098cfb25a81f4dc5f5ae5aa26011
만선 <xkznxh96@naver.com>
2024-10-16 17:59:57 +0900

    finish

--- data_crawling.py
+++ data_crawling.py
@@ -1,5 +1,3 @@
-import os
-import requests
 import pandas as pd
 import asyncio
 import aiohttp
@@ -12,32 +10,24 @@ name_selector = '#dev-content-wrap > article > section.content-recruit.on > arti
 title_selector = '#dev-content-wrap > article > section.content-recruit.on > article.list > article > div.list-section-information > div > a'
 history_selector = '#dev-content-wrap > article > section.content-recruit.on > article.list > article > div.list-section-information > ul.chip-information-group'
 
-# def get_company_url(links):
-#     result_list = []
-#     for l in links:
-#         res = requests.get(l , headers={"user-agent":user_agent})
-#         if res.status_code == 200:
-#             soup = BeautifulSoup(res.text, "lxml")
-#             name_list = soup.select(name_selector)
-            
-#             for name in name_list:
-#                 link = name.get("href")
-#                 result_list.append('https://www.jobkorea.co.kr/'+link)
-#         else:
-#             raise Exception(f"요청 실패. 응답코드: {res.status_code}")
-    
-#     return result_list
-
 async def get_company_info(url, session):
     async with session.get(url) as res:
-        result_list = []
+        all_results = []
         if res.status == 200:
             html = await res.text()
             soup = BeautifulSoup(html, "lxml")
-            name = soup.select(name_selector).get_text() 
-            title = soup.select(title_selector).get_text()
-            history = soup.select(history_selector).get_text()
-            result_list = [name , title , history]
+
+            name_list = soup.select(name_selector)
+            title_list = soup.select(title_selector)
+            history_list = soup.select(history_selector)
+
+            name = [n.get_text().strip() for n in name_list]
+            title = [t.get_text().strip() for t in title_list]
+            history = [h.get_text().strip().replace('\n','') for h in history_list]
+            
+            result_list = list(zip(name , title , history))
+            all_results.extend(result_list)
+            return all_results
         else:
             raise Exception(f"요청 실패. 응답코드: {res.status_code}")
     
@@ -54,11 +44,14 @@ async def main(links):
 if __name__ == '__main__':
     question = input("궁금하신 직무를 입력하세요 : ")
     pages = [f'https://www.jobkorea.co.kr/Search/?stext={question}&tabType=recruit&Page_No='+ str(x) for x in range(1,11)]
-    # pages_link = get_company_url(pages)
-    pages_data = asyncio.run(main(pages))
-    info_df = pd.DataFrame(pages_data)
 
-    d = datetime.now().strftime("%Y-%m-%d")
+    pages_data = asyncio.run(main(pages))
+    
+    flat_data = [item for subdata in pages_data for item in subdata]
+
+    info_df = pd.DataFrame(flat_data, columns=['회사명', '직무명', '상세정보'])
+
+    d = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 
     info_file_path = f"Datas/{d}.csv"
     info_df.to_csv(info_file_path, index=False)
